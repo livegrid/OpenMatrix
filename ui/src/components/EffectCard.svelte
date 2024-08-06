4a1e9e4b6e38b77841e75fbe6a912cbf02ad149e
Ayush Sharma <asrocks5@gmail.com>
2024-08-06 20:33:16 +0530

    feat: effect card shadow on hover

--- ui/src/components/EffectCard.svelte
+++ ui/src/components/EffectCard.svelte
@@ -3,7 +3,7 @@
   export let image, name, selected, loading;
 </script>
 
-<div class="relative flex flex-row gap-x-6 overflow-hidden rounded-md border border-gray-200 dark:border-zinc-900 bg-white/80 dark:bg-black/30 px-4 py-5 sm:px-6 sm:py-6">
+<div class="relative flex flex-row gap-x-6 overflow-hidden rounded-md border border-gray-200 dark:border-zinc-900 bg-white/80 dark:bg-black/30 px-4 py-5 sm:px-6 sm:py-6 hover:shadow-2xl transition-shadow duration-300 ease-in-out hover:shadow-emerald-500/20 dark:hover:shadow-emerald-900/50">
   <SelectButton on:click loading={loading} selected={selected} />
   <div>
     <svelte:component this={image} />
