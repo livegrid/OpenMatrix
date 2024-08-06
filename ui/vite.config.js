538306d4055260d60f0c0761a9af3dc2fc9ee1c0
Ayush Sharma <asrocks5@gmail.com>
2024-08-06 17:27:43 +0530

    feat: added mock API server

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -2,12 +2,18 @@ import path from 'path';
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { viteSingleFile } from "vite-plugin-singlefile"
+import { viteMockServe } from 'vite-plugin-mock'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     svelte(),
-    viteSingleFile()
+    viteSingleFile(),
+    viteMockServe({
+      // default
+      mockPath: 'mock',
+      enable: true,
+    }),
   ],
   resolve: {
     alias: {
