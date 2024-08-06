e7a7276b5749b5a2fffe2a56d3d33441eb5e31ce
Ayush Sharma <asrocks5@gmail.com>
2024-08-06 21:34:01 +0530

    fix: improved play button style

--- ui/src/components/PlayButton.svelte
+++ ui/src/components/PlayButton.svelte
@@ -12,14 +12,14 @@
         <Spinner size="m" />
     {:else}
         {#if active}
-            <button disabled class="flex flex-row items-center uppercase gap-x-3 text-emerald-600 dark:text-emerald-700">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-4 h-4"><polygon points="6 3 20 12 6 21 6 3"/></svg>
-            Playing
+            <button disabled class="py-1 px-2 flex flex-row items-center text-sm gap-x-1.5 rounded-full bg-emerald-50 dark:bg-emerald-900/40 border border-emerald-500 dark:border-emerald-700 text-emerald-600 dark:text-emerald-500 disabled:opacity-80">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-4 h-4"><polygon points="6 3 20 12 6 21 6 3"/></svg>
+                Playing
             </button>
         {:else}
-            <button on:click class="flex flex-row items-center uppercase gap-x-3 text-gray-400 hover:text-gray-500 transition-colors">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-4 h-4"><polygon points="6 3 20 12 6 21 6 3"/></svg>
-            Play
+            <button on:click class="py-1 px-2 flex flex-row items-center text-sm gap-x-1.5 rounded-full bg-gray-50 dark:bg-zinc-900 border border-gray-300 hover:bg-gray-100 hover:border-gray-400 dark:border-zinc-700 text-gray-500 dark:text-zinc-500 hover:text-gray-500 dark:hover:text-zinc-400 dark:hover:bg-zinc-800 dark:hover:border-zinc-600 transition-colors disabled:opacity-80">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-4 h-4"><polygon points="6 3 20 12 6 21 6 3"/></svg>
+                Play
             </button>
         {/if}
     {/if}
