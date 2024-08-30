96ab5c7b0c8ec240a40a9cf89db56d0ecc0077f8
Ayush Sharma <asrocks5@gmail.com>
2024-08-30 15:27:06 +0530

    feat: added new callbacks in webserver class

--- lib/WebServer/WebServerManager.cpp
+++ lib/WebServer/WebServerManager.cpp
@@ -121,6 +121,7 @@ void WebServerManager::setupInterface() {
       effectManager->setEffect(effect - 1);
       stateManager->getState()->effects.selected = effect;
       stateManager->getState()->mode = OpenMatrixMode::EFFECT;
+      stateManager->save();
       log_i("Effect changed to: %d", static_cast<int>(effect));
     }
   });
@@ -129,6 +130,7 @@ void WebServerManager::setupInterface() {
     if (imageDraw->openGIF(fileName.c_str())) {
       stateManager->getState()->image.selected = fileName;
       stateManager->getState()->mode = OpenMatrixMode::IMAGE;
+      stateManager->save();
       log_i("Image changed to: %s", fileName.c_str());
     } else {
       log_e("Failed to open GIF: %s", fileName.c_str());
@@ -144,8 +146,64 @@ void WebServerManager::setupInterface() {
     stateManager->getState()->text.payload = payload;
     stateManager->getState()->text.size = size;
     stateManager->getState()->mode = OpenMatrixMode::TEXT;
+    stateManager->save();
     // TODO: Update matrix text
     log_i("Text changed to: (%d) %s", size, payload.c_str());
+  });
+
+  interface.onMqttSettings([this](
+    const char* host,
+    uint16_t port,
+    const char* client_id,
+    const char* username,
+    const char* password,
+    const char* co2_topic,
+    const char* matrix_text_topic,
+    bool show_text
+  ) {
+    stateManager->getState()->settings.mqtt.host = host;
+    stateManager->getState()->settings.mqtt.port = port;
+    stateManager->getState()->settings.mqtt.client_id = client_id;
+    stateManager->getState()->settings.mqtt.username = username;
+    stateManager->getState()->settings.mqtt.password = password;
+    stateManager->getState()->settings.mqtt.co2_topic = co2_topic;
+    stateManager->getState()->settings.mqtt.matrix_text_topic = matrix_text_topic;
+    stateManager->getState()->settings.mqtt.show_text = show_text;
+    stateManager->save();
+
+    // TODO: Do something with these MQTT settings     
+  });
+
+  interface.onDmxSettings([this](
+    eDmxProtocol protocol,
+    eDmxMode mode,
+    bool multicast,
+    bool start_universe,
+    uint16_t start_address,
+    uint16_t timeout
+  ) {
+    stateManager->getState()->settings.dmx.protocol = protocol;
+    stateManager->getState()->settings.dmx.mode = mode;
+    stateManager->getState()->settings.dmx.multicast = multicast;
+    stateManager->getState()->settings.dmx.start_universe = start_universe;
+    stateManager->getState()->settings.dmx.start_address = start_address;
+    stateManager->getState()->settings.dmx.timeout = timeout;
+    stateManager->save();
+
+    // TODO: Do something with these DMX settings
+  });
+
+  interface.onHomeAssistantSettings([this](
+    bool show_text,
+  ) {
+    stateManager->getState()->settings.home_assistant.show_text = show_text;
+    stateManager->save();
+
+    // TODO: Do something with these Home Assistant settings
+  });
+
+  interface.onNetworkReset([this]() {
+    // TODO: Reset network (NetWizard)
   });
 }
 
